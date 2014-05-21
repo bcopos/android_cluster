2b3b1c3d81bfec12b7bc137b258b77bd82cb91f8
Michael Bierma <michaelbierma@gmail.com>
2014-05-20 22:27:29 -0700

    fixed paths in rootfs_glibc

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-qemu-system-x86_64 -kernel images/5.19/bzImage.64 -initrd images/rootfs/rootfs.ext3 -append "root=/dev/ram session_id=1 node_id=1"
+qemu-system-x86_64 -kernel images/5.19/bzImage.64 -initrd images/rootfs/rootfs.ext3 -append "ramdisk_size=128000 root=/dev/ram session_id=1 node_id=1"
 #qemu-system-x86_64 -kernel images/5.19/bzImage -initrd /Applications/Android/system-images/android-19/x86/system.img -append "session_id=1 node_id=1"
